a0bdb89cb9c1d621888feade014c2f29fd00f859
gruvert <147984995+gruvert@users.noreply.github.com>
2024-03-19 13:28:02 +0200

    Додані requests templates  і опис помилок при реєстрації чи логіні

--- main_f.py
+++ main_f.py
@@ -1,33 +1,42 @@
-from fastapi import FastAPI, HTTPException
-import uvicorn
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import Rest_DB
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 class User(BaseModel):
     name: str
     short_description: str
-    adress: str
+    address: str
     count_places: int
     email: str
     password: str
 
-@app.post("/register/")
-async def register(resto_data: User):
+@app.post("/register/", response_class=HTMLResponse)
+async def register(request: Request, resto_data: User):
     if any(resto_data.email == user['email'] for user in Rest_DB.get_info("a")):
         raise HTTPException(status_code=400, detail="This email is already registered")
-    Rest_DB.add_user(resto_data.name, resto_data.short_description, resto_data.adress,
-resto_data.count_places, resto_data.email, resto_data.password)
-    return {"message": "Restaurant registered successfully", "restaurant": resto_data}
+    Rest_DB.add_user(resto_data.name, resto_data.short_description, resto_data.address,
+                     resto_data.count_places, resto_data.email, resto_data.password)
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Restaurant registered successfully", "restaurant": resto_data})
 
-@app.post("/login/")
-async def login(resto_data: User):
+@app.get("/Log in/", response_class=HTMLResponse)
+async def login(request: Request, resto_data: User):
     for index, resto in enumerate(Rest_DB.get_info("a")): # resto is a dict
         if resto['email'] == resto_data.email:
             if resto['password'] == resto_data.password:
                 restaurant = Rest_DB.get_info(index)
-                return {"message": "Successful login", "restaurant": restaurant}
+                return templates.TemplateResponse("login.html", {"request": request, "message": "Successful login", "restaurant": restaurant})
             raise HTTPException(status_code=401, detail="Invalid password")
         raise HTTPException(status_code=401, detail="Invalid email")
-    return {"message": "Successful login", "restaurant": restaurant}
+    return templates.TemplateResponse("log_in.html", context={"request": request, "message": "Successful login", "restaurant": restaurant})
+# print(Rest_DB.get_info(0))
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+    
+
+
